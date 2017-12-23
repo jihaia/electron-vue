cbb21faa000948bb675ed1439c6a69caec25e54d
Jason Ihaia <jason@buildit.io>
2017-12-23 09:22:29 -0700

    Simplified sample landing page

--- template/src/renderer/App.vue
+++ template/src/renderer/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="app">
-    {{#isEnabled plugins 'vue-router'}}
+<div id="app" style="position: relative;">
+  <h1 class="ms-font-su ms-fontColor-white ms-bgColor-purple noselect" style="margin: 0; padding: 80px 20px 20px 20px; -webkit-app-region: drag; position: fixed; top: 0; width: 100%; z-index: 10000">
+    {{ name }}
+    <div style="font-size: 16px; margin-top: -5px;">Lorem ipsum dolar sit</div>
+  </h1>
+  <div style="padding-top: 170px;">
+    <navigator></navigator>
     <router-view></router-view>
-    {{else}}
-    <landing-page></landing-page>
-    {{/isEnabled}}
   </div>
+</div>
 </template>
 
 <script>
@@ -26,5 +29,32 @@
 </script>
 
 <style>
-  /* CSS */
+  .noselect {
+  cursor: pointer;
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background: #fafafa;
+}
+
+.header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 20px;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 10;
+}
 </style>
